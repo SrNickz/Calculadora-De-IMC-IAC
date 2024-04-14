9b97399e96fd4a289779e48a81db205a92c07aeb
Nicolas Jorge <srnick31@gmail.com>
2024-04-14 20:36:26 -0300

    First Commit

---
+++ CalculoIMCeIAC/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿namespace CalculoIMCeIAC {
+    internal class Pessoa {
+
+        public double Altura { get; set; }
+        public double MassaCorporal { get; set; }
+        public double CircunferenciaDoQuadril { get; set; }
+
+        public Pessoa(double massaCorporalOuCircunferencia, double altura) {
+            MassaCorporal = massaCorporalOuCircunferencia;
+            CircunferenciaDoQuadril = massaCorporalOuCircunferencia;
+            Altura = altura;
+        }
+
+        public double Imc(double massa, double altura) {
+            double imc = massa / (Math.Pow(altura, 2));
+            return imc;
+        }
+
+        public double Iac(double circunferencia, double altura) {
+            double iac = (circunferencia / (altura * (Math.Sqrt(altura)))) - 18;
+            return iac;
+        }
+    }
+}
---
+++ CalculoIMCeIAC/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using CalculoIMCeIAC;
+
+namespace Exercicio {
+    class Program {
+        static void Main(string[] args) {
+
+            char refazCalculo;
+
+            do {
+                Console.Clear();
+                Console.WriteLine("CALCULADORA DE IMC E IAC");
+                Console.WriteLine("\n------------------------\n");
+
+                Console.Write("Você deseja calcular seu IMC ou seu IAC? ");
+                string escolha = Console.ReadLine();
+
+                while (escolha != "IMC" && escolha != "imc" && escolha != "IAC" && escolha != "iac") {
+                    Console.Write("Entrada errada, por favor, digite novamente \n Entre com \"IMC\" ou \"IAC\": ");
+                    escolha = Console.ReadLine();
+                }
+
+                Console.WriteLine("\n------------------------\n");
+
+                if (escolha == "IMC" || escolha == "imc") {
+                    Console.WriteLine("Digite sua massa corporal e sua altura \n * Por favor, use o ponto ao invés da vírgula como separador decimal.");
+
+                    Console.WriteLine();
+
+                    Console.Write("Massa: ");
+                    double massa = double.Parse(Console.ReadLine());
+                    Console.Write("Altura: ");
+                    double altura = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\n------------------------\n");
+
+                    Pessoa pessoa = new Pessoa(massa, altura);
+                    double imc = pessoa.Imc(pessoa.MassaCorporal, pessoa.Altura);
+                    Console.WriteLine("Você tem um IMC de " + imc.ToString("F1") + " Kg/m²");
+
+                    if (imc < 18.5) {
+                        Console.WriteLine("Peso abaixo do normal!");
+                    }
+                    else if (imc <= 24.9) {
+                        Console.WriteLine("Peso normal, parabéns!");
+                    }
+                    else if (imc <= 29.9) {
+                        Console.WriteLine("Sobrepeso!");
+                    }
+                    else if (imc <= 34.9) {
+                        Console.WriteLine("Obesidade grau 1!");
+                    }
+                    else if (imc <= 39.9) {
+                        Console.WriteLine("Obesidade grau 2!");
+                    }
+                    else if (imc <= 49.9) {
+                        Console.WriteLine("Obesidade grau 3! Cuidado.");
+                    }
+                    else if (imc <= 59.9) {
+                        Console.WriteLine("Obesidade grau 4!");
+                    }
+                    else {
+                        Console.WriteLine("Obesidade grau 5! Procure ajuda médica.");
+                    }
+                    Console.WriteLine();
+                }
+                else {
+                    Console.WriteLine("Digite a circunferenicia do seu quadril e sua altura \n * Por favor, use o ponto ao invés da vírgula como separador decimal.");
+
+                    Console.WriteLine();
+
+                    Console.Write("Circunferencia: ");
+                    double circunferencia = double.Parse(Console.ReadLine());
+                    Console.Write("Altura: ");
+                    double altura = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\n------------------------\n");
+
+                    Pessoa pessoa = new Pessoa(circunferencia, altura);
+                    double iac = pessoa.Iac(pessoa.CircunferenciaDoQuadril, pessoa.Altura);
+                    Console.WriteLine("Você tem um IAC de " + iac.ToString("F1") + "%");
+                    Console.WriteLine();
+                }
+
+                Console.Write("Você deseja refazer a operação? (s/n) ");
+                refazCalculo = char.Parse(Console.ReadLine());
+                while (refazCalculo != 's' && refazCalculo != 'S' && refazCalculo != 'n' && refazCalculo != 'N') {
+                    Console.Write("Por favor, digite um parâmetro válido: ");
+                    refazCalculo = char.Parse(Console.ReadLine());
+                }
+            } while (refazCalculo == 's');
+        }
+    }
+}
